d7c3405e100d3fc1bf668708072e5ede8050ac44
adritakakoty <adrita.mitmpl2024@learner.manipal.edu>
2026-01-13 18:40:18 +0530

    Fix image paths and base URL for deployment

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -3,12 +3,11 @@ import React, { useState } from 'react';
 const Gallery = () => {
   const [lightbox, setLightbox] = useState({ isOpen: false, src: "" });
 
-  const sliderData = [
-    ["1.jpeg", "2.jpeg", "3.jpeg", "4.jpeg"],
-    ["12.jpeg", "5.jpeg", "6.jpeg", "7.jpeg"],
-    ["8.jpeg", "9.jpeg", "10.jpeg", "11.jpeg"]
-  ];
-
+ const sliderData = [
+    ["./1.jpeg", "./2.jpeg", "./3.jpeg", "./4.jpeg"],
+    ["./12.jpeg", "./5.jpeg", "./6.jpeg", "./7.jpeg"],
+    ["./8.jpeg", "./9.jpeg", "./10.jpeg", "./11.jpeg"]
+];
   const openLightbox = (src) => setLightbox({ isOpen: true, src });
 
   return (
